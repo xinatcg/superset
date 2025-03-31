82b3bfa3bfbbeb411e90561dfe7751fd258acd50
Evan Rusackas <evan@rusackas.com>
2025-03-30 21:22:33 -0600

    more changes

--- superset-frontend/src/components/ErrorMessage/ErrorAlert.tsx
+++ superset-frontend/src/components/ErrorMessage/ErrorAlert.tsx
@@ -17,11 +17,11 @@
  * under the License.
  */
 import { useState } from 'react';
-import { Tooltip } from 'src/components/Tooltip';
-import Modal from 'src/components/Modal';
-import Icons from 'src/components/Icons';
-import Alert from 'src/components/Alert';
 import { t, useTheme } from '@superset-ui/core';
+import { Tooltip } from '../Tooltip';
+import Modal from '../Modal';
+import Icons from '../Icons';
+import Alert from '../Alert';
 
 export interface ErrorAlertProps {
   errorType?: string; // Strong text on the first line
@@ -110,7 +110,7 @@ const ErrorAlert: React.FC<ErrorAlertProps> = ({
     <Alert
       description={renderDescription()}
       type={type}
-      showIcon
+      showIcon={showIcon}
       closable={closable}
       className={className}
     >
